fe591a478ccf23afbbd3305fa3b183894c967b8c
christian julius guillen <juliusguillen@gmail.com>
2022-03-19 08:28:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+    let win = record.find(function(record) {return record.result === "W"})
+    return(win.year? "W" : "undefined");
+    //return win.year;
+};
+console.log(year)
